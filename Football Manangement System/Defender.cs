e280a42d6da72935318bf6ced674b8970104f8fd
MmoaazM <imoaaz29@gmail.com>
2026-07-21 15:24:38 +0300

    Finish Defender Class

---
+++ Football Manangement System/Defender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class Defender : FootballPlayer
+    {
+        public int Tackles { get; set; }
+        public int Clearances { get; set; }
+
+        public Defender(string Name, int JerseyNumber, int Age, bool IsInjured,int Tackles,int Clearances):base(Name, JerseyNumber, Age, IsInjured)
+        {
+            this.Tackles = Tackles;
+            this.Clearances = Clearances;
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"The Defender {Name} is Playing");
+        }
+
+        public override void Train()
+        {
+            Console.WriteLine($"The Defender {Name} is training");
+        }
+    }
+}
