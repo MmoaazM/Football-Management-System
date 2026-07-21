c4cd9b6aa1e91657fc0926addb2f443136360908
MmoaazM <imoaaz29@gmail.com>
2026-07-21 19:02:52 +0300

    Finish TODO 1

--- Football Manangement System/Program.cs
+++ Football Manangement System/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+using Football_Manangement_System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("--- 1. CREATING PLAYERS (OOP) ---\n");
+
+        var salah = new Forward("Mohamed Salah", 10, 31, false,100);
+        var trezeguet = new Forward("Trezeguet", 7, 29, false,110);
+        var hegazi = new Defender("Ahmed Hegazi", 6, 32, false, 30,100);
+        var marmoush = new Forward("Omar Marmoush", 14, 24, false, 200);
+        var elShenawy = new GoalKeeper("Mohamed El-Shenawy", 1, 33,false, 12);
+
+
+
+
+        Console.WriteLine("--- 2. POLYMORPHISM ---\n");
+
+        List<FootballPlayer> players = new List<FootballPlayer> { salah, trezeguet, hegazi, marmoush, elShenawy };
+
+        foreach (var player in players)
+        {
+            player.Play();  
+            player.Train(); 
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("--- 3. CREATING TEAM ---\n");
+
+        var team = new Team("Al Ahly SC", "Marcel Koller");
+        team.AddPlayer(salah);
+        team.AddPlayer(trezeguet);
+        team.AddPlayer(hegazi);
+        team.AddPlayer(marmoush);
+        team.AddPlayer(elShenawy);
+
+        team.DisplaySquad();
+    }
+}
+
---
+++ Football Manangement System/Team.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public List<FootballPlayer> Players;
+        public string CoachName { get; set; }
+
+        public Team(string Name,string CoachName)
+        {
+            this.Name = Name;
+            this.CoachName = CoachName;
+            Players = new List<FootballPlayer>();
+        }
+
+        public void AddPlayer(FootballPlayer newPlayer)
+        {
+            if (!Players.Contains(newPlayer))
+            {
+                Players.Add(newPlayer);
+                Console.WriteLine($"The New User {newPlayer} Added Successfully");
+                return;
+            }
+
+            Console.WriteLine("The Player Already Exists");
+                
+        }
+        public void RemovePlayer(int JerseyNumber) 
+        {
+            var player = Players.FirstOrDefault(p => p.JerseyNumber == JerseyNumber);
+            if (player != null)
+            {
+                Players.Remove(player);
+                Console.WriteLine($"The Player {player.Name} Has Been Removed successfully");
+            }
+        } 
+        public void DisplaySquad()
+        {
+            Console.WriteLine("All Players Info :-");
+            foreach(var player in Players)
+            {
+                Console.WriteLine($"{player.JerseyNumber} {player.Name} ------> Age {player.Age} - IsInjured {player.IsInjured} - Number Of Goals {player.Goals} - Number Of Assists {player.Assists}");
+            }
+        }
+        public float CalculateAverageAge()
+        {
+            if (Players.Count == 0)
+            {
+                Console.WriteLine("The Team Is Empty");
+                return 0;
+            } 
+            float sumOfAge = 0;
+            foreach(var player in Players)
+            {
+                sumOfAge += player.Age;
+            }
+            return sumOfAge / Players.Count;
+        }
+
+        public int GetTotalGoals() =>Players.Sum(p => p.Goals);
+        public FootballPlayer GetTopScorer()
+        {
+            FootballPlayer topScorer=Players.FirstOrDefault();
+            foreach(var player in Players)
+            {
+                if (player.Goals > topScorer.Goals)
+                    topScorer = player;
+            }
+            return topScorer;
+        }
+        
+    }
+}
