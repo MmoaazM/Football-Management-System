0746a2d866f60d596a4697dcecb51ae5f8c618de
MmoaazM <imoaaz29@gmail.com>
2026-07-21 15:19:06 +0300

    Finish GoalKeeper Class

---
+++ Football Manangement System/GoalKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class GoalKeeper : FootballPlayer
+    {
+        public int CleanSheets { get; set; }
+        public GoalKeeper(string Name, int JerseyNumber, int Age, bool IsInjured,int CleanSheets) : base(Name, JerseyNumber, Age, IsInjured)
+        {
+            this.CleanSheets = CleanSheets;
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"The GoalKeeper {Name} is Playing");
+        }
+        public override void Train()
+        {
+            Console.WriteLine($"The GoalKeeper {Name} is Training");
+        }
+    }
+}
