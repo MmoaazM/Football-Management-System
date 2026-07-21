4df3e3fb11fa7fb95bb3ebba9ab101893d1985e4
MmoaazM <imoaaz29@gmail.com>
2026-07-21 15:32:05 +0300

    Finish Forward & Midfielder Classes

---
+++ Football Manangement System/Forward.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class Forward : FootballPlayer
+    {
+        public int ShotsOnTarget { get; set; }
+        public Forward(string Name, int JerseyNumber, int Age, bool IsInjured,int ShotsOnTarget) : base(Name, JerseyNumber, Age, IsInjured)
+        {
+            this.ShotsOnTarget = ShotsOnTarget;
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"The Forward {Name} is Playing");
+        }
+        public override void Train()
+        {
+            Console.WriteLine($"The Forward {Name} is Training");
+        }
+    }
+}
---
+++ Football Manangement System/Midfielder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class Midfielder : FootballPlayer
+    {
+        public int Passes { get; set; }
+        public int KeyPasses { get; set; }
+        public Midfielder(string Name, int JerseyNumber, int Age, bool IsInjured,int Passes,int KeyPasses) : base(Name, JerseyNumber, Age, IsInjured)
+        {
+            this.Passes = Passes;
+            this.KeyPasses = KeyPasses;
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"The Midfielder {Name} is Playing");
+        }
+        public override void Train()
+        {
+            Console.WriteLine($"The Midfielder {Name} is Training");
+        }
+    }
+}
