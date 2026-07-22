6f4748646279ff9dd5d5ca669f3bbf9773c37096
MmoaazM <imoaaz29@gmail.com>
2026-07-23 00:11:08 +0300

    Finish TODO 2 (Indexer & Generics)

---
+++ Football Manangement System/Squad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class Squad<T> where T:FootballPlayer // All Methods Here Need Implimentation
+    {
+        private List<T> _players;
+        public void AddPlayer(T player) { }
+        public void RemovePlayer(int jerseyNumber) { }
+        public List<T> GetPlayersByPosition(string position) { return null; }
+        public List<T> GetTopScorers(int count) { return null; }
+        public int GetAverageRating() { return 0; }
+    }
+}
---
+++ Football Manangement System/StatsCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class StatsCalculator<T> where T : FootballPlayer
+    {
+        public int CalculateAverageGoals(List<T> players) { return 0; }
+        public int CalculateAverageAge(List<T> players) { return 0; }
+        public FootballPlayer GetBestPlayer(List<T> players) { return null; }
+        public FootballPlayer GetWorstPlayer(List<T> players) { return null; }
+
+    }
+}
