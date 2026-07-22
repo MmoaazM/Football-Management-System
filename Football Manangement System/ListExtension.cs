5f15969d7727fde3672906abaec182e87c4bf0a4
MmoaazM <imoaaz29@gmail.com>
2026-07-23 01:17:19 +0300

    Finish Extension Methods of Player and List

---
+++ Football Manangement System/ListExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public static class ListExtension
+    {
+        public static void Shuffle<T>(this List<T> list) { }
+        public static T GetTop<T>(this List<T> list, int count, Func<T, int> selector) {  }
+        public static T GetRandom<T>(this List<T> list, int count) { }
+    }
+}
---
+++ Football Manangement System/PlayerFilterExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public static class PlayerFilterExtensions
+    {
+        public static bool IsStarPlayer(this FootballPlayer player) => player.Goals > 20;
+        public static bool IsVeteran(this FootballPlayer player) => player.Age > 30;
+        public static bool IsInjured(this FootballPlayer player) => player.IsInjured;
+        public static bool IsGoalKeeper(this FootballPlayer player) => player is GoalKeeper;
+        public static bool HasMinimumGoals(this FootballPlayer player, int minGoals) => player.Goals <= minGoals;
+        public static bool ByPosition(this FootballPlayer player, string position) => player.GetType().Name.Equals(position, StringComparison.OrdinalIgnoreCase);
+    }
+}
