5f91b121b258125949ff367bc1f1396c94db6f74
MmoaazM <imoaaz29@gmail.com>
2026-07-23 12:16:13 +0300

    finish Team Analyzer Class

---
+++ Football Manangement System/TeamAnalyzer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class TeamAnalyzer
+    {
+        public int CalculateTeamStat(List<FootballPlayer>players,Func<FootballPlayer,int>selector)
+        {
+            int result=0;
+            foreach(var player in players)
+            {
+                result += selector(player);
+            }
+            return result;
+        }
+
+        public void ApplyToAllPlayers(List<FootballPlayer> players, Action<FootballPlayer> applyChange)
+        {
+            foreach (var player in players)
+                applyChange(player);
+        }
+
+        public List<FootballPlayer> FindPlayers(List<FootballPlayer> players, Predicate<FootballPlayer> filter)
+        {
+            var returnedPlayers = new List<FootballPlayer>();
+            foreach(var player in players)
+            {
+                if (filter(player))
+                {
+                    returnedPlayers.Add(player);
+                }
+            }
+            return returnedPlayers;
+        }
+    }
+}
