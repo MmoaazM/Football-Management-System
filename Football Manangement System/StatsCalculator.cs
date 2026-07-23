1329bfb86422b8f1fa02bb6b19498870129f12a5
MmoaazM <imoaaz29@gmail.com>
2026-07-23 18:27:59 +0300

    Finish StatsCalculator Class

--- Football Manangement System/StatsCalculator.cs
+++ Football Manangement System/StatsCalculator.cs
@@ -6,10 +6,56 @@ namespace Football_Manangement_System
 {
     public class StatsCalculator<T> where T : FootballPlayer
     {
-        public int CalculateAverageGoals(List<T> players) { return 0; }
-        public int CalculateAverageAge(List<T> players) { return 0; }
-        public FootballPlayer GetBestPlayer(List<T> players) { return null; }
-        public FootballPlayer GetWorstPlayer(List<T> players) { return null; }
+        public int CalculateAverageGoals(List<T> players) 
+        {
+            int sumOfGoals = 0;
+            foreach(var player in players)
+            {
+                sumOfGoals += player.Goals;
+            }
+            return sumOfGoals / players.Count;
+        }
+        public int CalculateAverageAge(List<T> players) 
+        {
+            int sumOfAges = 0;
+            foreach (var player in players)
+            {
+                sumOfAges += player.Age;
+            }
+            return sumOfAges / players.Count;
+        }
+        public FootballPlayer GetBestPlayer(List<T> players)
+        {
+            int sumOfGoalsAndAssists = players[0].Goals+players[0].Assists;
+            var sum=0;
+            T bestPlayer=players[0];
+            foreach(var player in players)
+            {
+                sum = player.Goals + player.Assists;
+                if (sum > sumOfGoalsAndAssists)
+                {
+                    sumOfGoalsAndAssists = sum;
+                    bestPlayer = player;
+                }
+            }
+            return bestPlayer;
+        }
+        public FootballPlayer GetWorstPlayer(List<T> players) 
+        {
+            int sumOfGoalsAndAssists = players[0].Goals + players[0].Assists;
+            var sum = 0;
+            T worstPlayer = players[0];
+            foreach (var player in players)
+            {
+                sum = player.Goals + player.Assists;
+                if (sum < sumOfGoalsAndAssists)
+                {
+                    sumOfGoalsAndAssists = sum;
+                    worstPlayer = player;
+                }
+            }
+            return worstPlayer;
+        }
 
     }
 }
