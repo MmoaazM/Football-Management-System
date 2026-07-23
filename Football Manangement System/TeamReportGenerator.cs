902e17834ef674615d98f3786243b2c1867e2c22
MmoaazM <imoaaz29@gmail.com>
2026-07-23 11:13:36 +0300

    finish TeamReportGenerator (it uses delegate)

---
+++ Football Manangement System/TeamReportGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Manangement_System
+{
+    public class TeamReportGenerator
+    {
+        public delegate bool PlayerFilter(FootballPlayer p);
+        public void GenerateReport(List<FootballPlayer>players,string title,PlayerFilter filter)
+        {
+            Console.WriteLine($"Team Report for {title} is being Generated ...");
+            var matchedPlayers = new List<FootballPlayer>();
+
+            foreach(var player in players)
+            {
+                if (filter(player))
+                {
+                    matchedPlayers.Add(player);
+                }
+            }
+
+            Console.WriteLine("Displaying Matching Players .....");
+
+            foreach(var player in players)
+            {
+                Console.WriteLine($"Name : { player.Name}  JerseyNumber  : {player.JerseyNumber}");
+            }
+        }
+    }
+}
