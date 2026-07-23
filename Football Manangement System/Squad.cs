8802be8e04b93a19c8de43db1d8762b31ab41b43
MmoaazM <imoaaz29@gmail.com>
2026-07-23 18:18:21 +0300

    Finish Squad Class

--- Football Manangement System/Squad.cs
+++ Football Manangement System/Squad.cs
@@ -7,10 +7,44 @@ namespace Football_Manangement_System
     public class Squad<T> where T:FootballPlayer // All Methods Here Need Implimentation
     {
         private List<T> _players;
-        public void AddPlayer(T player) { }
-        public void RemovePlayer(int jerseyNumber) { }
-        public List<T> GetPlayersByPosition(string position) { return null; }
-        public List<T> GetTopScorers(int count) { return null; }
-        public int GetAverageRating() { return 0; }
+        public void AddPlayer(T player)
+        {
+            if (_players.Contains(player)) return;
+            _players.Add(player);
+        }
+        public void RemovePlayer(int jerseyNumber)
+        {
+            T playerToRemove = _players.FirstOrDefault(p => p.JerseyNumber == jerseyNumber);
+            if (playerToRemove == null)
+                return;
+            _players.Remove(playerToRemove);
+        }
+        public List<T> GetPlayersByPosition(string position) 
+        {
+            List<T> matchedPlayer = new();
+            foreach(var player in _players)
+            {
+                if (player.GetType().Name.Equals(position, StringComparison.OrdinalIgnoreCase))
+                {
+                    matchedPlayer.Add(player);
+                }
+            }
+            return matchedPlayer;
+        }
+        public List<T> GetTopScorers(int count) 
+        {
+            List<T> matchedPlayer = new();
+            _players.Sort((a, b) => b.Goals.CompareTo(a.Goals));
+            for(int i = 0; i < count; i++)
+            {
+                matchedPlayer.Add(_players[i]);
+            }
+
+            return matchedPlayer;
+        }
+        public int GetAverageRating() // How To Implement This ??
+        {
+            return 0;
+        }
     }
 }
